b0727a744d8683e7784012d3f6e71b86255b1040
sairamya07-hub <manjumanjula6619@gmail.com>
2026-01-17 16:55:09 +0530

    Add form handling with POST request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,12 @@ def home():
 @app.route("/about")
 def about():
     return "Hi, I am learning Flask and building backend projects."
+@app.route("/greet", methods=["GET", "POST"])
+def greet():
+    if request.method == "POST":
+        name = request.form["name"]
+        return render_template("greet.html", name=name)
+    return render_template("greet.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
