9d0704ac148e335d59e16ac377234ac188fbe246
sairamya07-hub <manjumanjula6619@gmail.com>
2026-01-17 16:41:44 +0530

    Pass variable from Flask to template

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    name = "Sai"
+    return render_template("index.html", name=name)
+
+@app.route("/about")
+def about():
+    return "Hi, I am learning Flask and building backend projects."
 
 if __name__ == "__main__":
     app.run(debug=True)
